ad13d6a310c0765e901e547efdfcb1a1616b9b93
acristhian1411 <acristhian35@gmail.com>
2025-09-29 21:03:37 -0300

    fix(fees): correcciones en formularios de créditos y cuotas
    - Se corrigió el formulario de cuotas para obtener correctamente las cuotas en edición
    - Se integró el formulario de cuotas dentro del de créditos
    - Se agregaron pasos en el formulario de créditos para guiar al usuario y evitar sobrecarga de inputs

--- frontend/src/routes/Credits/Form.jsx
+++ frontend/src/routes/Credits/Form.jsx
@@ -4,35 +4,50 @@ import {
   Button,
   TextField,
   Typography,
-  Stack,
+  Stepper,
+  Step,
+  StepLabel,
   Grid,
   Autocomplete,
-  CircularProgress
 } from "@mui/material";
-import {api} from "../../api";
-import { formatNumber,disFormatNumber } from "../../utils/formatNumbers";
-import FormFees  from "../Fees/Form";
+import { api } from "../../api";
+import { formatNumber, disFormatNumber } from "../../utils/formatNumbers";
+import FormFees from "../Fees/Form";
 
-export default function FormCredit({onClose, edit, credit, showAlert}) {
+export default function FormCredit({ onClose, edit, credit, showAlert }) {
   let date = new Date();
   const [formData, setFormData] = useState({
     id: credit?.id || "",
     client_id: credit?.client_id || "",
-    client_name: credit?.client_name || '',
+    client_name: credit?.client_name || "",
     amount: credit?.amount || "",
     fees_qty: credit?.fees_qty || "",
     fee_amount: credit?.fee_amount || "",
     interest_rate: credit?.interest_rate || "",
     start_date: credit?.start_date || date.toISOString().split("T")[0],
     status: credit?.status || "",
-    loading: false
+    loading: false,
   });
-  const [clients,setClients]= useState([])
+  const [activeStep, setActiveStep] = useState(0);
+  const steps = ["Datos del crédito", "Cuotas"];
+  const [clients, setClients] = useState([]);
+
+  const handleNext = (e) => {
+    e?.preventDefault();
+    if (activeStep === 0) {
+      handleSubmit();
+    }
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
 
   const handleFeeAmountChange = (e) => {
     // const value = parseFloat(disFormatNumber(e.target.value));
-    const value = e.target.value
-    setFormData(f => ({ ...f, fee_amount: value }));
+    const value = e.target.value;
+    setFormData((f) => ({ ...f, fee_amount: value }));
   };
 
   /**
@@ -43,15 +58,15 @@ export default function FormCredit({onClose, edit, credit, showAlert}) {
    * @return {Promise<void>} - A Promise that resolves when the state variable
    * has been updated.
    */
-  const searchClient = async (e) => {
-    const results = await api.clients.search(e.target.value);
+  const searchClient = async (searchTerm) => {
+    const results = await api.clients.search(searchTerm);
     setClients(results);
   };
 
   const loadClients = async () => {
-    setClients(await api.clients.list())
-  }
-  
+    setClients(await api.clients.list());
+  };
+
   /**
    * Updates the 'client_id' and 'client_name' fields of the 'formData' state
    * variable based on the selected client.
@@ -61,18 +76,18 @@ export default function FormCredit({onClose, edit, credit, showAlert}) {
    * properties.
    * @return {void} This function does not return anything.
    */
-  function handleClientChange(event, value) { 
-    setFormData((prev) => ({       
-      ...prev,       
-      client_id: value?.id || "",       
-      client_name: value?.name ? `${value?.name} ${value?.lastname}` : "",     
-    }));   
+  function handleClientChange(event, value) {
+    setFormData((prev) => ({
+      ...prev,
+      client_id: value?.id || "",
+      client_name: value?.name ? `${value?.name} ${value?.lastname}` : "",
+    }));
   }
 
   const handleCreate = async (e) => {
     e.preventDefault();
     try {
-      await api.credits.create(formData)
+      await api.credits.create(formData);
       showAlert("Crédito creado exitosamente", "success");
     } catch (err) {
       console.error(err);
@@ -92,131 +107,166 @@ export default function FormCredit({onClose, edit, credit, showAlert}) {
 
   const handleSubmit = edit ? handleEdit : handleCreate;
 
-  useEffect(()=>{
-    loadClients()
-  },[])
+  useEffect(() => {
+    loadClients();
+  }, []);
 
   return (
     <Box
       component="form"
-      onSubmit={handleSubmit}
+      onSubmit={(e) => handleNext(e)}
       sx={{ maxWidth: 900, mx: "auto", mt: 4 }}
     >
       <Typography variant="h5" gutterBottom align="center" sx={{ mb: 3 }}>
         {edit ? "Editar Crédito" : "Crear nuevo Crédito"}
       </Typography>
-      {/* <Stack spacing={2}> */}
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}>    
-            {/* 
+      <Box sx={{ width: "500px" }}>
+        <Stepper activeStep={activeStep} sx={{ mb: 3 }}>
+          {steps.map((label, index) => {
+            const stepProps = {};
+            const labelProps = {};
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+        {activeStep === 0 && (
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              {/* 
             // TODO: Implementar búsqueda de clientes
             */}
-            <Box sx={{width:'235px'}}>
-              <Autocomplete           
-                options={clients}           
-                sx={{width: '100%'}}
-                getOptionLabel={(option) => option.name+" "+ option.lastname}           
-                onChange={handleClientChange}           
-                // fullWidth
-                onInputChange={(event, newInputValue, reason) => {             
-                  if(reason === 'input') {               
-                    searchClient(newInputValue);             }
-                }}           
-                value={             
-                  clients.find((c)=> c.id === formData.client_id) || null           
-                }           
-                renderInput={(params) => (             
-                  <TextField               
-                    {...params}               
-                    label="Cliente"               
-                    name="client_id"               
-                    // fullWidth
-                    required               
-                    // InputProps={{                 
-                    //   ...params.InputProps,                 
-                    //   endAdornment: (                   
-                    //     <React.Fragment>                     
-                    //       {params.InputProps.endAdornment}                     
-                    //       {formData.loading ? (                       
-                    //         <CircularProgress color="inherit" size={20} />                     
-                    //         ) : null}                  
-                    //     </React.Fragment>                 
-                    //   ),               
-                    // }}             
-                  />           
-                )}         
+              <Box sx={{ width: "235px" }}>
+                <Autocomplete
+                  options={clients}
+                  sx={{ width: "100%" }}
+                  getOptionLabel={(option) =>
+                    option.name + " " + option.lastname
+                  }
+                  onChange={handleClientChange}
+                  // fullWidth
+                  onInputChange={(event, newInputValue, reason) => {
+                    if (reason === "input") {
+                      searchClient(newInputValue);
+                    }
+                  }}
+                  value={
+                    clients.find((c) => c.id === formData.client_id) || null
+                  }
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Cliente"
+                      name="client_id"
+                      // fullWidth
+                      required
+                    />
+                  )}
+                />
+              </Box>
+            </Grid>
+            <Grid item xs={3} sm={3}>
+              <TextField
+                label="Monto total"
+                amount="amount"
+                type="number"
+                value={formData.amount}
+                onChange={(e) =>
+                  setFormData((f) => ({ ...f, amount: e.target.value }))
+                }
+                required
+                fullWidth
               />
-            </Box>
-                
+            </Grid>
+            <Grid item xs={3} sm={3}>
+              <TextField
+                label="Cantidad de cuotas"
+                amount="fees_qty"
+                type="number"
+                value={formData.fees_qty}
+                onChange={(e) =>
+                  setFormData((f) => ({
+                    ...f,
+                    fees_qty: e.target.value,
+                    fee_amount: (
+                      parseFloat(formData.amount) / parseFloat(e.target.value)
+                    ).toFixed(2),
+                  }))
+                }
+                required
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={3} sm={3}>
+              <TextField
+                label="Monto por cuota"
+                amount="fee_amount"
+                type="text"
+                value={formData.fee_amount}
+                onChange={handleFeeAmountChange}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Tasa de interés"
+                amount="interest_rate"
+                type="number"
+                value={formData.interest_rate}
+                onChange={(e) =>
+                  setFormData((f) => ({ ...f, interest_rate: e.target.value }))
+                }
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Fecha de inicio"
+                amount="start_date"
+                type="date"
+                sx={{ width: "235px" }}
+                value={formData.start_date}
+                onChange={(e) =>
+                  setFormData((f) => ({ ...f, start_date: e.target.value }))
+                }
+                fullWidth
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={3} sm={3}>        
-            <TextField
-              label="Monto total"
-              amount="amount"
-              type="number"
-              value={formData.amount}
-              onChange={e => setFormData(f => ({ ...f, amount: e.target.value }))}
-              required
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={3} sm={3}>        
-            <TextField
-              label="Cantidad de cuotas"
-              amount="fees_qty"
-              type="number"
-              value={formData.fees_qty}
-              onChange={e => setFormData(f => ({ ...f, fees_qty: e.target.value }))}
-              required
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={3} sm={3}>        
-            <TextField
-              label="Monto por cuota"
-              amount="fee_amount"
-              type="text"
-              value={formData.fee_amount}
-              onChange={handleFeeAmountChange}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>            
-            <TextField
-              label="Tasa de interés"
-              amount="interest_rate"
-              type="number"
-              value={formData.interest_rate}
-              onChange={e => setFormData(f => ({ ...f, interest_rate: e.target.value }))}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>        
-            <TextField
-              label="Fecha de inicio"
-              amount="start_date"
-              type="date"
-              sx={{ width: '235px' }}
-              value={formData.start_date}
-              onChange={e => setFormData(f => ({ ...f, start_date: e.target.value }))}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>        
-            <FormFees creditId={formData.id} creditData={formData} edit={!edit} showTitle={false} />
-          </Grid>
+        )}
+        {activeStep === 1 && (
+          <container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <FormFees
+                creditId={formData.id}
+                creditData={formData}
+                edit={edit}
+                showTitle={false}
+              />
+            </Grid>
+          </container>
+        )}
+        <Grid item xs={12} sm={12} m={3}>
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            sx={{ mr: 2 }}
+          >
+            Siguiente
+          </Button>
+          <Button
+            variant="contained"
+            type="reset"
+            color="error"
+            onClick={onClose}
+          >
+            Cancelar
+          </Button>
         </Grid>
-          <Grid item xs={12} sm={6}>        
-            <Button variant="contained" color="primary" type="submit" sx={{ mr: 2 }}>
-              {edit ? "Editar" : "Crear"}
-            </Button>
-            <Button variant="contained" type="reset" color="error" onClick={onClose}>
-              Cancelar
-            </Button>
-          </Grid>
-          
-        
-      {/* </Stack> */}
+      </Box>
     </Box>
   );
 }
--- frontend/src/routes/Fees/Form.jsx
+++ frontend/src/routes/Fees/Form.jsx
@@ -10,45 +10,77 @@ import {
   TableCell,
   TableBody,
   IconButton,
-  Tooltip
+  Tooltip,
 } from "@mui/material";
-import DeleteIcon from '@mui/icons-material/Delete';
-import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
 import { api } from "../../api";
 
-function FormFees({ creditId, setOpenModal, fee, edit, showTitle, creditData }) {
-  const [formData, setFormData] = useState(
-    [
-      {
-        amount: fee?.amount || '',
-        expirate_at: fee?.expirate_at || '',
-        credit_id: fee?.credit_id || '',
-      }
-    ]
-  );
+function FormFees({
+  creditId,
+  setOpenModal,
+  fee,
+  edit,
+  showTitle,
+  creditData,
+}) {
+  const [formData, setFormData] = useState([]);
 
   useEffect(() => {
-    if(!edit && creditData){
-        const currentFeesLength = formData.length;
-        const remainingFees = creditData.fees_qty - currentFeesLength;
-        for (let i = 0; i < remainingFees; i++) {
-            addFees((creditData.fee_amount * (1 + creditData.interest_rate / 100)).toFixed(2));
-        }
+    let mount = true;
+    let feesArray = [];
+    if (!edit && creditData) {
+      console.log("cosas");
+      const currentFeesLength = formData.length;
+      const remainingFees = creditData.fees_qty - currentFeesLength;
+      for (let i = 0; i < remainingFees; i++) {
+        addFees(
+          (
+            creditData.fee_amount *
+            (1 + creditData.interest_rate / 100)
+          ).toFixed(2)
+        );
+      }
+    } else if (edit && creditId) {
+      Promise.resolve(api.fees.listByCredit(creditId))
+        .then((fee) => {
+          const feesArray = fee.map((element) => ({
+            amount: element.amount,
+            expirate_at: element.expirate_at,
+            credit_id: creditId,
+          }));
+          if (mount) {
+            setFormData(feesArray);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
     //  fee && setFormData(fee);
-  }, [creditData]);
+    return () => {
+      mount = false;
+    };
+  }, [creditData, creditId, edit]);
 
   const handleChange = (e, index) => {
-    setFormData(f => f.map((fee, i) => i === index ? { ...fee, [e.target.name]: e.target.value } : fee));
+    setFormData((f) =>
+      f.map((fee, i) =>
+        i === index ? { ...fee, [e.target.name]: e.target.value } : fee
+      )
+    );
   };
 
   const addFees = (amount = 0) => {
-    console.log('amount', amount);
-    setFormData(f => [...f, { amount: amount, expirate_at: '', credit_id: creditId }]);
+    console.log("amount", amount);
+    setFormData((f) => [
+      ...f,
+      { amount: amount, expirate_at: "", credit_id: creditId },
+    ]);
   };
 
   const removeFees = (index) => {
-    setFormData(f => f.filter((fee, i) => i !== index));
+    setFormData((f) => f.filter((fee, i) => i !== index));
   };
 
   const handleSave = async (e) => {
@@ -68,77 +100,82 @@ function FormFees({ creditId, setOpenModal, fee, edit, showTitle, creditData }) 
   };
 
   return (
-    <Box component="form" onSubmit={handleSave} noValidate sx={{ mt: 4 }}>
-      {
-        showTitle && (
-          <Typography variant="h5" gutterBottom align="center" sx={{ mb: 3 }}>
-            {
-              edit ? 'Editar Cuotas' : 'Crear cuotas'
-            }
-          </Typography>
-        )
-      }
+    <Box onSubmit={handleSave} sx={{ mt: 4 }}>
+      {showTitle && (
+        <Typography variant="h5" gutterBottom align="center" sx={{ mb: 3 }}>
+          {edit ? "Editar Cuotas" : "Crear cuotas"}
+        </Typography>
+      )}
       <Table>
         <TableHead>
           <TableRow>
             <TableCell>Monto</TableCell>
             <TableCell>Fecha de vencimiento</TableCell>
-            <TableCell colSpan={2}>{''}</TableCell>
+            <TableCell colSpan={2}>{""}</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {formData.length > 0 && formData.map((fee, index) => (
-          <TableRow key={index}>
-
-          <TableCell>
-            <TextField
-              autoComplete="amount"
-              name="amount"
-              sx={{ width: '170px' }}
-              required
-              fullWidth
-              id="amount"
-              label={`Cuota ${index + 1}`}
-              autoFocus
-              value={fee.amount}
-              onChange={(e) => handleChange(e, index)}
-            />
-          </TableCell>
-          <TableCell>
-            <TextField
-              required
-              fullWidth
-              id="due_date"
-              label="Fecha de vencimiento"
-              name="due_date"
-              type="date"
-              defaultValue={fee.due_date}
-              InputLabelProps={{
-                shrink: true,
-              }}
-              onChange={(e) => handleChange(e, index)}
-            />
-          </TableCell>
-          <TableCell>
-            <Tooltip title="Agregar cuota">
-              <IconButton variant="outlined" color="primary" onClick={addFees} sx={{ mr: 2 }}>
-                  <AddIcon />
-                </IconButton>
-            </Tooltip>
-          </TableCell>
-          <TableCell>
-            {formData.length > 1 && (
-              <Tooltip title="Eliminar cuota">
-              <IconButton variant="outlined" color="secondary" onClick={() => removeFees(index)} sx={{ mr: 2 }}>
-                <DeleteIcon />
-              </IconButton>
-              </Tooltip>
-            )}
-          </TableCell>
-        </TableRow>
-        ))}
+          {formData.length > 0 &&
+            formData.map((fee, index) => (
+              <TableRow key={index}>
+                <TableCell>
+                  <TextField
+                    autoComplete="amount"
+                    name="amount"
+                    sx={{ width: "170px" }}
+                    required
+                    fullWidth
+                    id="amount"
+                    label={`Cuota ${index + 1}`}
+                    autoFocus
+                    value={fee.amount}
+                    onChange={(e) => handleChange(e, index)}
+                  />
+                </TableCell>
+                <TableCell>
+                  <TextField
+                    required
+                    fullWidth
+                    id="due_date"
+                    label="Fecha de vencimiento"
+                    name="due_date"
+                    type="date"
+                    defaultValue={fee.expirate_at}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    onChange={(e) => handleChange(e, index)}
+                  />
+                </TableCell>
+                <TableCell>
+                  <Tooltip title="Agregar cuota">
+                    <IconButton
+                      variant="outlined"
+                      color="primary"
+                      onClick={addFees}
+                      sx={{ mr: 2 }}
+                    >
+                      <AddIcon />
+                    </IconButton>
+                  </Tooltip>
+                </TableCell>
+                <TableCell>
+                  {formData.length > 1 && (
+                    <Tooltip title="Eliminar cuota">
+                      <IconButton
+                        variant="outlined"
+                        color="secondary"
+                        onClick={() => removeFees(index)}
+                        sx={{ mr: 2 }}
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    </Tooltip>
+                  )}
+                </TableCell>
+              </TableRow>
+            ))}
         </TableBody>
-
       </Table>
       <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
         Guardar
